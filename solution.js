7b538736bafb2af82d35540d65ec606835d8fbde
Michael Caldwell <michaelcaldwell@pursuit.org>
2024-01-16 22:22:14 -0500

    Update Lab

--- solution.js
+++ solution.js
@@ -1,4 +1,46 @@
-const { nums, words } = require("./data/data.js");
+const { nums, words } = require( "./data/data.js" );
+
+class Node
+{
+  constructor(data, next) {
+    this.data = data
+    this.next = null
+    
+  }
+  
+}
+
+class LinkedList
+{
+  constructor ( head = null )
+  {
+    this.head = head;
+  }
+
+  size ()
+  {
+    let count = 0;
+    let node = this.head;
+    
+    while ( node )
+    {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+}
+
+let previousNode = new Node( 11 );
+let numList = new LinkedList( previousNode );
+
+for (let i = 0; i < nums.length; i++) {
+  let currentNode = new Node( nums[ i ] );
+  previousNode.next = currentNode;
+  previousNode = currentNode
+}
+
+console.log(numList.size());
 
 module.exports = {
   Node,
